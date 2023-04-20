a5a1c608f6e58f8b72ccefe098b9734b8366281b
Marketmare <marmaresova@gmail.com>
2023-04-20 12:53:45 +0200

    homework1

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -3,7 +3,14 @@ describe('Homework', async () => {
     it('should open page and create screenshot', async () => {
 
         // sem vypracuj domácí úkol
+        await browser.reloadSession('chrome');
 
+        await browser.url('/registrace');
+        
+        await browser.saveScreenshot('signup_page.png');
+
+        const windowSize = await browser.getWindowSize();
+        console.log(windowSize);
     });
 
 });
