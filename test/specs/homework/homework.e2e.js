8707da2e9104334378753a5787b7cfc3088c5dde
Marketmare <marmaresova@gmail.com>
2023-05-10 08:03:44 +0200

    Homework_02

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -2,7 +2,7 @@ describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
 
-        // sem vypracuj domácí úkol
+        // sem vypracuj domácí úkol (Lekce_01)
         await browser.reloadSession('chrome');
 
         await browser.url('/registrace');
@@ -11,6 +11,49 @@ describe('Homework', async () => {
 
         const windowSize = await browser.getWindowSize();
         console.log(windowSize);
+    
+/* 
+(Lekce_02)
+
+Najdi nejvhodnější:
+1. Políčko pro jméno a příjmení id="name"
+2. Políčko pro email id="email"
+3. Políčko pro zadání hesla id="password"
+4. Políčko pro kontrolu zadaného hesla id="password-confirm"
+5. Tlačítko na registraci class="btn btn-primary"
+
+Dobrovolné:
+Tvým úkolem je si zatím jen selektory najít a otestovat v Developer Tools. 
+Pokud se na to cítíš, můžeš zkusit zapsat najít elementy a zapsat je do homework.e2e.js 
+podobně jako jsme to dělali ve cvičení.
+
+*/
+    
+        const nameField = await $('#name');
+        console.log(await nameField.getHTML ()) ;
+
+        var nameField2 = await $('#name');
+        console.log(await nameField2.getHTML ()) ;
+        console.log(await nameField2.getLocation ()) ;
+
+        const emailField = await $('#email');
+        console.log(await emailField.getHTML());
+
+        const passwordField = await $('#password');
+        console.log(await passwordField.getHTML());
+
+        const passConfField = await $('#password-confirm');
+        console.log (await passwordField.getHTML());
+
+        const regButton = await $('.btn-primary');
+        console.log (await regButton.getText ());
+    
     });
 
 });
+
+
+
+
+
+
